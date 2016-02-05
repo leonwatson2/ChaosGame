8f3813d867fc4ca889b09e1af02ad6a6f1cb2385
Leon <vernonwatzon@yahoo.com>
2016-02-04 21:25:26 -0600

    Final Project Upload

--- prettyTriangle.c
+++ prettyTriangle.c
@@ -14,10 +14,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-float v1[] = {.65,.8}, 
-      v2[] = {.12,.15}, 
-      v3[] = {.9,.25};
+float v[][2] = {{.65,.8},{.12,.15},{.9,.25}};
 float p[] = {.25,.25};
+
 
 void reshape(int w, int h){
    glViewport(0, 0, w, h);
@@ -33,43 +32,53 @@ void keypress(unsigned char key, int x, int y){
          
    }
 }
-/*
-*  Sets new midpoint 
-*/
-void midpoint(float vert[]){
+void midpoint(float v[]){
    
-   p[0] = (vert[0] + p[0])/2;
+   p[0] = (v[0] + p[0])/2;
    if(p[0] < 0) p[0] = p[0]*-1;
 
-   p[1] = (vert[1] + p[1])/2;
+   p[1] = (v[1] + p[1])/2;
    if(p[1] < 0) p[1] = p[1]*-1;
 }
-
 /*
-*  Draws a point on the screen 
-*  with color white.
+* Return absolute value of a float passed
 */
-void drawPoint(float vert[]){
-         glBegin(GL_POINTS);
-            glColor3f(1.0,1.0,1.0);
-            glVertex2d(vert[0], vert[1]);
-         glEnd();
-   
+float absL(float x){
+   if(x < 0) return x * -1;
+
+   return x;
 }
 /*
-*  Chaos Game rules implemented here using 
-*  global
+* Adds pretty color effect to the Sierpinski Triangle 
 */
+void makeItPretty(float vert[]){
+   float start = v[1][0],
+         end = v[2][0];
+   float length = end - start; 
+   float third = start + length/3,
+         twoThird = start + 2*length/3;
+         
+
+   float red = ((third - start)/2) - ((p[0]- start) / (third - start)),
+         green = ((twoThird - start)/2) - ((p[0]-third) / (twoThird - third)),
+         blue =  ((end - twoThird)/2) - ((p[0]-twoThird) / (end - twoThird));
+                  
+   glColor3f (absL(red), absL(green), absL(blue));
+}
+
+void drawPoint(float vert[]){
+   glColor3f(0,0,0);
+   makeItPretty(vert);
+   glVertex2d (vert[0], vert[1]);
+   
+}
 void chaosAlgorithm(){
       for (int i = 0; i < 60000; i++)
       {
-         int r = rand() % 3;
-         if(r == 0) midpoint(v1);
-         else if(r == 1) midpoint(v2);
-         else midpoint(v3);
-
-         drawPoint(p);
-
+         midpoint(v[rand() % 3]);
+         glBegin(GL_POINTS);
+            drawPoint(p);
+         glEnd();
       }
 
 }
@@ -78,9 +87,26 @@ void display(void)
 /* clear all pixels  */
    glClear (GL_COLOR_BUFFER_BIT);
 
-/* draw points using Chaos Algorithm  */
+/* draw initial vector points
+ */
+   for (int i = 0; i < sizeof(v)/sizeof(v[0]); ++i)
+   {
+      glBegin(GL_POINTS);
+         glColor3f (1.0, 0.0, 1.0);
+         glVertex2d(v[i][0],v[i][1]);   
+      glEnd();
+   }
+   glBegin(GL_POINTS);
+         glColor3f (0.0, 1.0, 1.0);
+         glVertex2d(p[0],p[1]);   
+   glEnd();
+
    chaosAlgorithm();
    
+   
+/* don't wait!  
+ * start processing buffered OpenGL routines 
+ */
    glFlush ();
 }
 
@@ -99,10 +125,9 @@ void init (void)
 /* 
  * Declare initial window size, position, and display mode
 
- * (single buffer and RGBA).  Open window with "Chaos Game:Leon"
+ * (single buffer and RGBA).  Open window with "hello"
  * in its title bar.  Call initialization routines.
  * Register callback function to display graphics.
- * Register callback function for keypresses.
  * Enter main loop and process events.
  */
 int main(int argc, char** argv)
@@ -117,5 +142,5 @@ int main(int argc, char** argv)
    glutDisplayFunc(display);
    glutKeyboardFunc(keypress);
    glutMainLoop();
-   return 0;   
+   return 0;   /* ANSI C requires main to return int. */
 }
